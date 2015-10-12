76f5be4608241d672960f034ec5301891ccaf88b
rmarquez1 <albertocomp10@gmail.com>
2015-10-12 11:49:49 -0430

    Subiendo a git

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ process.env.MONGOLAB_URI ||
 process.env.MONGOHQ_URL ||
 'mongodb://localhost/officelogistics_db';
 
-
 // CONEXION A BASE DE DATOS
 var mongoose = require('mongoose');
 mongoose.connect(uristring, function(error){
